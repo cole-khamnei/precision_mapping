6fa1c5727b6043196cff2ee26a3b9504e5d1bf73
Cole Khamnei <cole.khamnei@gmail.com>
2025-02-20 12:53:56 -0500

    working; going to create pipeline branch

--- src/pipeline.py
+++ src/pipeline.py
@@ -1,18 +1,34 @@
-# from . import surface_mapping as sfm
-# from . import network_assigment as na
-# from . import plot
-# from . import constants
+import argparse
+import sys
 
+from . import constants
+from . import utils
+
+sys.path.insert(0, constants.PROJECT_PATH)
+import xmath_tools as xmt
 
 # ----------------------------------------------------------------------------# 
 # --------------------         Parallel Infomaps          --------------------# 
 # ----------------------------------------------------------------------------# 
 
-
-def parallel_infomaps():
+def get_arguments(test_args=None):
     """ """
-    raise NotImplementedError
+    parser.add_argument('-c', "--ciftis", dest="cifti_txt", type=str, required=True,
+                        help="Txt file with paths of cifti files")
+    args = parser.parse_args() if test_args is None else parser.parse_args(test_args)
 
+
+    return args
+
+
+def main(test_args=None):
+    """ """
+
+    args = get_arguments(test_args=test_args)
+
+
+if __name__ == '__main__':
+    main()
 
 # ----------------------------------------------------------------------------# 
 # --------------------                End                 --------------------# 
