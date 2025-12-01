b6129cf8bb6f7669a48b53f9e84fab3e7ad7ded5
Cole Khamnei <cole.khamnei@gmail.com>
2025-12-01 10:38:56 -0500

    forgot to added cifti_tools + partition_tools last commit :P

---
+++ src/partition_tools.py
@@ -0,0 +1,110 @@
+import numpy as np
+import scipy
+
+from . import constants, cifti_tools, utils
+from . import surface_mapping as sfm
+
+# ----------------------------------------------------------------------------# 
+# -              Load Functions For Precision Map Intermediates              -# 
+# ----------------------------------------------------------------------------# 
+
+
+def process_partition(partition, min_voxels=0, filter_subcortex=True):
+    """ """
+    index, groups = partition
+    if filter_subcortex:
+        # TODO: fix these issues, subcortex masking may not be correct
+        subcortex_index = index >= (32_492 * 2)
+        index, groups = index[~subcortex_index], np.unique(groups[~subcortex_index], return_inverse=True)[1]
+
+    groups = np.random.permutation(np.max(groups))[groups - 1]
+    unique_groups, counts = np.unique(groups, return_counts=True)
+    count_filter = np.isin(groups, unique_groups[counts >= min_voxels])
+
+    vertex_labels = np.full(np.max(index) + 1, fill_value=np.nan)
+    vertex_labels[index[count_filter]] = groups[count_filter]
+
+    return vertex_labels, (index, groups)
+
+
+def get_partition_cortex(partition, cifti):
+    """ """
+    vertex_labels, partition = process_partition(partition)
+    return cifti_tools.get_cortex_data(vertex_labels.reshape(1, -1), cifti)[0]
+
+
+def load_FC_cortex(FC_path, template_cifti):
+    """ """
+    sc = scipy.sparse.load_npz(FC_path)
+    cortex_index = cifti_tools.get_cortex_data(np.arange(sc.shape[0]).reshape(1, -1), template_cifti)[0]
+    return sc[cortex_index][:, cortex_index]
+
+
+def load_partition_labels(partition_path, template_cifti):
+    """ """
+    partition = np.load(partition_path)
+    vertex_labels = get_partition_cortex(partition, template_cifti)
+    vertex_labels[np.isnan(vertex_labels)] = np.nanmax(vertex_labels) + 1
+    remapped_vertex_labels = np.unique(vertex_labels, return_inverse=True)[1]
+    return remapped_vertex_labels
+
+
+def load_network_labels(network_path):
+    """ """
+    return np.load(network_path)[0].astype(int)
+
+
+# ----------------------------------------------------------------------------# 
+# -------------             Partition Dlabel Writers             -------------# 
+# ----------------------------------------------------------------------------# 
+
+
+def write_dlabel_precision_map(precision_map_values, save_path, label="", **kwargs):
+    """ """
+    precision_map_labels = precision_map_values.copy()
+    precision_map_labels["left"] = precision_map_labels["left"].astype(str)
+    precision_map_labels["right"] = precision_map_labels["right"].astype(str)
+    sfm.write_labels_to_dlabel(precision_map_labels, save_path, label_name=label, **kwargs)
+
+
+def write_parcel_dlabel(parcel_partition_path, parcel_dlabel_path, template_cifti, cmap=None):
+    """ """
+    # list of paths version
+    args = [parcel_partition_path, parcel_dlabel_path]
+    if utils.check_multiple_args(args, main_dtype=str):
+        np.vectorize(write_parcel_dlabel)(*args, template_cifti=template_cifti, cmap=cmap)
+        return
+
+    template_cifti = cifti_tools.get_template_cifti(template_cifti)
+    vertex_parcel_labels = load_partition_labels(parcel_partition_path, template_cifti)
+
+    parcel_values_int = cifti_tools.cifti_map(None, vertex_parcel_labels, template_cifti)
+    write_dlabel_precision_map(parcel_values_int, parcel_dlabel_path, cmap=cmap)
+
+
+def write_network_dlabel(network_partition_path, network_dlabel_path, template_cifti,
+                        cmap=constants.NETWORK_CMAP):
+    """ """
+    # list of paths version
+    args = [network_partition_path, network_dlabel_path]
+    if utils.check_multiple_args(args, main_dtype=str):
+        np.vectorize(write_network_dlabel)(*args, template_cifti=template_cifti, cmap=cmap)
+        return
+
+    template_cifti = cifti_tools.get_template_cifti(template_cifti)
+    # single path version
+    vertex_network_labels, vertex_network_strings = np.load(network_partition_path)
+    vertex_network_labels = vertex_network_labels.astype(int)
+    network_values_int = cifti_tools.cifti_map(None, vertex_network_labels, template_cifti)
+    network_name_to_num_map = {z: vertex_network_strings[np.where(vertex_network_labels == z)[0][0]]
+                               for z in np.unique(vertex_network_labels)}
+    network_dlabel_values = {
+        "left": np.array(list(map(network_name_to_num_map.get, network_values_int["left"]))),
+        "right": np.array(list(map(network_name_to_num_map.get, network_values_int["right"])))
+    }
+    write_dlabel_precision_map(network_dlabel_values, network_dlabel_path, cmap=cmap)
+
+
+# ----------------------------------------------------------------------------# 
+# --------------------                End                 --------------------# 
+# ----------------------------------------------------------------------------#
