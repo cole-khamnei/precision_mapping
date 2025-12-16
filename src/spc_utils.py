b24e4183f37b06052ee7d9797f63085abe126e8b
Cole Khamnei <cole.khamnei@gmail.com>
2025-12-16 12:29:59 -0500

    rte

--- src/spc_utils.py
+++ src/spc_utils.py
@@ -86,6 +86,7 @@ def get_device_info(backend: Union[str, Module], device) -> dict:
 def backend_array(backend: Union[str, Module]):
     """ """
     backend_name = get_backend_name(backend)
+    print(backend_name)
     if backend_name == "numpy":
         return np.array
 
