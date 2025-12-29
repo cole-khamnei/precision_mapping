added kmeans option for parcel detection - working; fixed exclude index error / subcortical masking - seems to work; concer for indexing error / striping with infomaps method?

src/functional_connectivity.py
@@ -19,12 +19,13 @@ def generate_voxel_FC(voxel_data,
                       save_path=None,
                       sparsity=0.1,
                       exclude_index_path=None,
+                      filter_subcortex=True,
                       mask=None,
                       block_size=5_000,
                       leave=True,
                       **SC_kwargs):
     """ """
-    exclude_index = np.load(exclude_index_path) if exclude_index_path else None
+    
 
     if str(mask).isdigit():
         mask = constants.get_geodesic_distance_mask_path(int(mask))
@@ -39,6 +40,13 @@ def generate_voxel_FC(voxel_data,
 
     # TODO: Fix masking related issues
     # TODO: add infomaps check, to insure that at least a certain percent of vertices have connections
+
+    if exclude_index_path:
+        exclude_index = np.load(exclude_index_path)
+    elif filter_subcortex:
+        exclude_index = np.arange(voxel_data.shape[1]) >= 59_412
+    else:
+        exclude_index = None
 
     sc = SparseCorrelator.run(voxel_data, sparsity_percent=sparsity, symmetric=True,
                               mask=mask, exclude_index=exclude_index,

src/parcellate.py
@@ -5,14 +5,22 @@ import numpy as np
 import multiprocess as mp
 import scipy
 
+from sklearn.cluster import KMeans
 from infomap import Infomap
 from tqdm.auto import tqdm
 
-from . import utils
+from . import utils, cifti_tools
 
 # ----------------------------------------------------------------------------# 
 # ----------------           Infomaps Parcellating            ----------------# 
 # ----------------------------------------------------------------------------# 
+
+
+def save_partition(indices, groups, save_path, fill_value=np.nan):
+    """ """
+    full_indices, full_groups = np.arange(91_282).astype(int), np.full(91_282, fill_value=fill_value)
+    full_groups[indices] = groups
+    np.save(save_path, [full_indices, full_groups])
 
 
 def infomap_parcellation(matrix, save_path=None, num_trials=1, **kwargs):
@@ -31,16 +39,63 @@ def infomap_parcellation(matrix, save_path=None, num_trials=1, **kwargs):
     infomap.run()
     partition = infomap.get_modules()
 
-    index = np.array(list(partition.keys()))
-    values = np.array(list(partition.values()))
+    indices = np.array(list(partition.keys()))
+    groups = np.array(list(partition.values()))
+
 
     if save_path:
-        np.save(save_path, [index, values])
+        save_partition(indices, groups, save_path)
     
-    return index, values
+    return indices, groups
 
 
-#TODO: add support for other clustering methods/parcellating methods
+# \section kmeans parcellating
+
+
+def kmeans_parcel_detection(dtseries_paths, parcel_save_paths, censor_files, 
+                            overwrite, seed, n_parcels,
+                            filter_subcortex=True):
+    """ """
+
+    dtseries_paths = utils.list_wrap(dtseries_paths, str)
+    parcel_save_paths = utils.list_wrap(parcel_save_paths, str)
+    censor_files = utils.list_wrap(censor_files, str)
+
+    assert len(dtseries_paths) == len(parcel_save_paths)
+    if censor_files is not None:
+        assert len(censor_files) == len(parcel_save_paths)
+    else:
+        censor_files = [None] * len(parcel_save_paths)
+
+    desc = "Running kmeans parcel detection ({})"
+    pbar = tqdm(total=len(parcel_save_paths), desc=desc)
+    for dtseries_path, parcel_save_path, censor_file in zip(dtseries_paths, parcel_save_paths, censor_files):
+        sample_label = parcel_save_path.split("/")[-1].split("_kms")[0]
+        pbar.set_description(desc.format(sample_label))
+
+        voxel_data = cifti_tools.load_voxel_data(dtseries_path, censor_file=censor_file)
+        indices = np.arange(voxel_data.shape[1])
+
+        if filter_subcortex:
+            # TODO: identify correct subcortical masking, believe 59_412 is correct
+            subcortex_indices = indices >= 59_412
+            indices, voxel_data = indices[~subcortex_indices], voxel_data[:, ~subcortex_indices]
+
+            kmeans_l = KMeans(n_clusters=n_parcels + 1, random_state=seed).fit(voxel_data.T[:29_696])
+            kmeans_r = KMeans(n_clusters=n_parcels + 1, random_state=seed).fit(voxel_data.T[29_696:59_412])
+
+            labels = np.hstack([kmeans_l.labels_, kmeans_r.labels_])
+            save_partition(indices, labels, parcel_save_path)
+        else:
+            kmeans = KMeans(n_clusters=n_parcels + 1, random_state=seed, n_init="auto").fit(voxel_data.T)
+            save_partition(indices, kmeans.labels_, parcel_save_path)
+        pbar.update(1)
+
+    pbar.close()
+
+
+# \section generic parcellation methods
+
 
 def parcel_detection_single(corr_matrix, save_path, n_reps=1, silent=True,
                             overwrite=False, seed=137, **kwargs):
@@ -70,6 +125,7 @@ def parcel_detection(corr_matrix, save_path, n_cores=None, silent=True,
 
     results = [single_parcel_func(arg_set) for arg_set in tqdm(arg_sets, total=len(save_paths), desc=desc)]
 
+    # TODO: debug multiprocess in jn?
     # n_cores = utils.get_n_cores(n_cores)
     # with mp.Pool(n_cores) as p:
     #     results = []

src/partition_tools.py
@@ -12,9 +12,10 @@ from . import surface_mapping as sfm
 def process_partition(partition, min_voxels=0, filter_subcortex=True):
     """ """
     index, groups = partition
+    index = index.astype(int)
     if filter_subcortex:
-        # TODO: fix these issues, subcortex masking may not be correct
-        subcortex_index = index >= (32_492 * 2)
+        # TODO: identify correct subcortical masking, believe 59_412 is correct
+        subcortex_index = index >= 59_412
         index, groups = index[~subcortex_index], np.unique(groups[~subcortex_index], return_inverse=True)[1]
 
     groups = np.random.permutation(np.max(groups))[groups - 1]